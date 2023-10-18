efd2c8569843f6faacecca17035adba9249ac866
DietherMacedo <140772398+DietherMacedo@users.noreply.github.com>
2023-10-17 21:12:54 -0600

    Console Logs

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+/*
+// String
+let nombre = "Angeles5";
+// Number
+let puntuaje = 15;
+//Boolean
+let hayComida = true
+// Undefined
+let perro
+// Null
+let dato = null
+*/
 
+console.log(puntuaje);
+console.log(hayComida);
