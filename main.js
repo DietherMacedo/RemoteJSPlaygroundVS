21f0ef891683758aae9ec07ba58759c6b082c2c8
DietherMacedo <140772398+DietherMacedo@users.noreply.github.com>
2023-10-20 17:46:31 -0600

    Console Logs

--- main.js
+++ main.js
@@ -13,3 +13,16 @@ let dato = null
 
 console.log(puntuaje);
 console.log(hayComida);
+
+let numero = 25
+let string = "First String"
+let boolean = false
+
+console.log(numero)
+console.log(string)
+console.log(boolean)
+
+console.log(numero,string,boolean)
+console.log(numero+string+boolean)
+console.log(numero + ", " + string + ", " + boolean)
+console.log(`${numero} , ${string}`)
